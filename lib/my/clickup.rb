0cb181a1538b8fc89439de4a72875b9b1c8551a3
nasum <tomato.wonder.life@gmail.com>
2022-03-04 16:58:10 +0900

    init

---
+++ lib/my/clickup.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "clickup/version"
+
+module My
+  module Clickup
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/my/clickup_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe My::Clickup do
+  it "has a version number" do
+    expect(My::Clickup::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
